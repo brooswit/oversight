3e91c2d34e275561a8c681bb03cd97d97ae09a1a
John Winstead <jwinstead@launchdarkly.com>
2020-05-14 16:34:01 +0000

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3334;
 
-app.use(express.static('./src/public/'));
+app.use(express.static('./public/'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
